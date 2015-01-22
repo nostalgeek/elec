1e217a84bece4f1a3aa2d8dc39a2dff51e7bf40b
ljacquemin <loic.jacquemin@hotmail.com>
2015-01-22 20:36:15 +0100

    Création de l'espace formulaire sur le plan html uniquement

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Elec | Connexion</title>
 
     <!-- Bootstrap -->
+    <link href="css/connect.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -17,7 +18,56 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+     <div class="container" style="margin-top:40px">
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <strong> Espace de connexion</strong>
+            </div>
+            <div class="panel-body">
+              <form role="form" action="#" method="POST">
+                <fieldset>
+                  <div class="row">
+                    <div class="center-block">
+                      <img class="profile-img"
+                        src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon">
+                            <i class="glyphicon glyphicon-user"></i>
+                          </span> 
+                          <input class="form-control" placeholder="Identifiant" name="loginname" type="text" autofocus>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon">
+                            <i class="glyphicon glyphicon-lock"></i>
+                          </span>
+                          <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <input type="submit" class="btn btn-lg btn-primary btn-block" value="Connexion">
+                      </div>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+            <div class="panel-footer ">
+              Pour la création d'un compte, contactez Samuel
+            </div>
+                  </div>
+        </div>
+      </div>
+    </div> 
+
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
