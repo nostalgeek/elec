334cb9874d8a95971dc2eabd23a10b1cb4715472
ljacquemin <loic.jacquemin@hotmail.com>
2015-01-23 02:06:02 +0100

    Création des autres fichier type modif.php etc...

---
+++ modifications.php
@@ -0,0 +1,45 @@
+<?php
+                $pageCurrent = substr(basename($_SERVER['PHP_SELF']), 0, -4);
+                
+            ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <?php
+        include('inc/head.inc.php');
+    ?>
+     <title>Elec | Modifications</title>
+    <title></title>
+  </head>
+  <body>
+     
+
+<?php
+include('inc/menu.inc.php');
+?>
+
+
+
+<div class="container">
+    <div class="navbar-template text-center">
+        <h1>Bootstrap NavBar</h1>
+        <p class="lead text-info">NavBar with too many childs.</p>
+        <a target="_blank" href="http://bootsnipp.com/snippets/featured/multi-level-dropdown-menu-bs3">Thanks to msurguy (Multi level dropdown menu BS3)</a>
+
+        <p>
+            
+                
+        </p>
+    </div>
+</div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+   
+
+  </body>
+</html>
